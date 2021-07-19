f761c85202dddbb4aa1704b4b6c12a2a6b7a5dfe
ofael <fael_assis@hotmail.com>
2021-07-19 20:37:23 -0300

    Function setContracts

---
+++ app/src/main/java/app/application/contatosbootcamp/Contact.kt
@@ -0,0 +1,6 @@
+package app.application.contatosbootcamp
+
+data class Contact(
+    val name : String,
+    val numTel : String
+)
--- app/src/main/java/app/application/contatosbootcamp/MainActivity.kt
+++ app/src/main/java/app/application/contatosbootcamp/MainActivity.kt
@@ -1,11 +1,49 @@
 package app.application.contatosbootcamp
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CONTACT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS),REQUEST_CONTACT)
+        } else{
+            setContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == REQUEST_CONTACT) setContacts()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun setContacts() {
+        val contactList : ArrayList<Contact> = ArrayList()
+
+        val cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+
+        if ( cursor != null){
+            while(cursor.moveToNext()){
+                contactList.add(Contact(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))))
+            }
+        }
+        cursor?.close()
+
     }
 }
